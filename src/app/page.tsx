8917427d2fd76e5877da23aabe140977e1369433
Hassaan Arain <rafatnaz21@gmail.com>
2024-11-14 10:38:42 +0500

    code pushed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,59 @@
+import Image from "next/image";
+import HomeImage from "../../public/images/Images/homepageimageforlearnhub.jpg";
+import Link from "next/link";
+import "../styles/HomePage.css";
+import TopCourses from "../components/TopCourses";
 
+// import OurStatus from "./components/OurStatus";
+// import Testenomials from "./components/Testenomials";
 export default function Home() {
   return (
-   <div>Hello worlD!</div>
+    <main>
+      <section className="firstSection">
+        <div className="customWidth  customSpacing  customFlexMd">
+          <h1 style={{ fontSize: "1.5rem", color: "#2563eb" }}>
+            Welcome to LearnHub – Your Gateway to Knowledge!
+          </h1>
+          <h3 style={{ fontSize: "1.25rem", color: "#1f2937" }}>
+            Unlock Your Potential, Anytime, Anywhere
+          </h3>
+          <p>
+            At LearnHub, we believe that learning should be accessible,
+            engaging, and empowering. With a wide range of courses designed by
+            experts, we make it easy for you to gain new skills and advance your
+            career, all at your own pace.
+          </p>
+          <div
+            className="customSpaceX4"
+            style={{ paddingTop: "0.75rem", paddingBottom: "0.75rem" }}
+          >
+            <Link href={"/courses"}>
+              {" "}
+              <button className="customHoverBlue"
+              style={{fontWeight:700,padding:"12px", color:"white"}}
+              >
+                Courses
+              </button>
+            </Link>
+            <Link href={"/contact"}>
+              {" "}
+              <button className="customButton">
+                Contact
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="customContainerbox">
+          <Image
+            src={HomeImage}
+            alt="HomeImage"
+            className="customWidth111"
+          />
+        </div>
+      </section>
+      <TopCourses/>
+    {/* <OurStatus/> */}
+    {/* <Testenomials/> */}
+    </main>
   );
 }
---
+++ src/components/TopCourses.tsx
@@ -0,0 +1,72 @@
+import Image from "next/image";
+import CourseWeb3 from "../../public/images/Images/web3.0imageforwebiste.jpg";
+import CourseAI from "../../public/images/Images/aiimageeforwebsite.webp";
+import CourseCC from "../../public/images/Images/cloudcomputingimageforwebsite.jpg";
+import "../app/courses/courses.css";
+export default function Courses() {
+  return (
+    <div
+      style={{
+        background: "#f7fafc",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <h2
+        style={{
+          color: "#2563eb",
+          fontSize: "1.875rem",
+          fontWeight: 700,
+          textAlign: "center",
+          paddingTop: "40px",
+          paddingBottom: "40px",
+        }}
+      >
+        Our Top Courses
+      </h2>
+
+      <main className="customGrid">
+        <section className="sectionCss">
+          <Image
+            src={CourseWeb3}
+            style={{ maxWidth: "100%", height: "auto" }}
+            alt="Courses"
+          />
+          <h3 className="sectionH3">Web 3.0</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum,
+            corporis. Explicabo iusto officia minima voluptates ratione
+            praesentium odit beatae itaque omnis facere,
+          </p>
+        </section>
+        <section className="sectionCss">
+          <Image
+            src={CourseAI}
+            style={{ maxWidth: "100%", height: "auto" }}
+            alt="Courses"
+          />
+          <h3 className="sectionH3">Artificial Intelligence</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum,
+            corporis. Explicabo iusto officia minima voluptates ratione
+            praesentium odit beatae itaque omnis facere,
+          </p>
+        </section>
+        <section className="sectionCss">
+          <Image
+            src={CourseCC}
+            style={{ maxWidth: "100%", height: "auto" }}
+            alt="Courses"
+          />
+          <h3 className="sectionH3">Cloud Computing</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum,
+            corporis. Explicabo iusto officia minima voluptates ratione
+            praesentium odit beatae itaque omnis facere,
+          </p>
+        </section>
+      </main>
+    </div>
+  );
+}
